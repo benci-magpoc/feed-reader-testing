6990550b6cbdbe954297b25897ffda65b948fc22
Benci Magpoc <benciian@gmail.com>
2019-02-14 03:34:32 -0500

    Test suites are written

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,72 +1,104 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
 
+  describe('RSS Feeds', function() {
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+    //checks if feeds are defined
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
     });
 
+    //checks if feed urls are defined
 
-    /* TODO: Write a new test suite named "The menu" */
+    it('each has url', function() {
+      for(let feed of allFeeds) {
+        expect(feed.url).toBeDefined();
+        expect(feed.url.constructor).toBe(String);
+        expect(feed.url.length).not.toBe(0);
+      }
+     });
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+     //checks if feed has a name defined
+     it('each has name', function() {
+       for(let feed of allFeeds) {
+         expect(feed.name).toBeDefined();
+         expect(feed.name.constructor).toBe(String);
+         expect(feed.name.length).not.toBe(0);
+       }
+     });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+  }); //end RSS Feeds test suite
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+  //The menu test suite
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+  describe('The Menu', function() {
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    //checks if menu is hidden by default
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    it('hidden by default', function() {
+      let menuIsHidden = document.body.classList.contains('menu-hidden');
+      expect(menuIsHidden).toBe(true);
+    });
+
+    //checks if menu is toggling
+
+    it('does menu toggles when clicked', function() {
+
+      let menuClick = document.querySelector('a.menu-icon-link');
+      menuClick.click();
+      expect(document.body.classList.contains('menu-hidden')).toBe(false);
+      menuClick.click();
+      expect(document.body.classList.contains('menu-hidden')).toBe(true);
+      });
+
+  }); //end Initial Entries test suite
+
+  //Initial Entries test suite
+
+  describe('Initial Entries', function() {
+
+    //setting up beforeEach
+
+    beforeEach(function(done) {
+      loadFeed(1, done);
+    });
+
+    //checks if feed has entries
+
+    it('has entries in feed', function() {
+      let feedContainer = document.querySelector('div.feed');
+      expect(feedContainer.children.length).toBeGreaterThan(0);
+    });
+
+  }); //end Initial Entries test suite
+
+  //New Feed Selection test suite
+
+  describe('New Feed Selection', function() {
+
+    //first and second variables to compare feeds
+
+    let first, second;
+
+    //setting up beforeEach
+
+    beforeEach(function(done) {
+      loadFeed(3, function() {
+        first = document.querySelector('div.feed').innerHTML;
+          loadFeed(2, function() {
+            second = document.querySelector('div.feed').innerHTML;
+            done();
+          });
+      });
+    });
+
+    //checks if feeds load correctly
+
+    it('feed loads fine', function() {
+      expect(first).not.toBe(second);
+    });
+
+  }); //end New Feed Selection test suite
+
 }());
